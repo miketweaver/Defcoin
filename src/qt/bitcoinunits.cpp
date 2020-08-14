b49ad74808f61c47ef5de0cfcd94abd986f74ac0
-k <slowdive@me.com>
2020-08-13 23:58:43 -0700

    🤕

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -44,7 +44,7 @@ QString BitcoinUnits::longName(int unit)
     {
     case BTC: return QString("DFC");
     case mBTC: return QString("lites");
-    case uBTC: return QString("photons");
+    case uBTC: return QString("tangents");
     case SAT: return QString("litoshi");
     default: return QString("???");
     }
@@ -66,7 +66,7 @@ QString BitcoinUnits::description(int unit)
     {
     case BTC: return QString("Defcoins");
     case mBTC: return QString("Lites (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Photons (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Tangents (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     case SAT: return QString("Litoshis (sat) (1 / 100" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
